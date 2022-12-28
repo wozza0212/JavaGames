d498472ad13be187463b3327418e20e7c7947fcd
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-12-28 20:06:29 +0000

    ball position

--- BrickBreaker/src/BB/Main.java
+++ BrickBreaker/src/BB/Main.java
@@ -9,9 +9,9 @@ public class Main {
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Brick Breaker");
         obj.setResizable(false);
-        obj.setVisible(true);
+
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
+        obj.add(gamePlay);
+        obj.setVisible(true);
     }
 }
