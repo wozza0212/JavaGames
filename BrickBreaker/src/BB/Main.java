51b2e3523bb41c214d8b6826bfef70dd5b3e24b3
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-12-28 18:10:49 +0000

    Added to main class

--- BrickBreaker/src/BB/Main.java
+++ BrickBreaker/src/BB/Main.java
@@ -1,7 +1,17 @@
 package BB;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 }
